375fe154706ae1859c8bdf04b4d4b74ffd1f679c
Muhammad Ahmed <m.ahmed.uh72@gmail.com>
2025-03-05 13:33:08 +0500

    Backend

---
+++ Class2/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+// const parse = require('parse');
+const bodyParser = require("body-parser");
+
+
+const app = express();
+// app.use(parse());
+app.use(bodyParser.urlencoded());
+
+
+app.get('/',(req,res)=>{
+    res.send("This is first API congrats!");
+});
+
+app.listen(5000,()=>{
+    console.log('server is running....')
+})
+
+app.post('/user',(req,res)=>{
+    const{email,password}=req.body;
+    res.send('email');
+});
